fead300ca62d43dcb85639b0f0a94bf11c5ebc24
DexterStorey <dexter@dexterstorey.com>
2025-05-31 12:42:10 -0400

    deprecate postinstall

--- cli.ts
+++ cli.ts
@@ -74,38 +74,38 @@ const cli = createCLI({
 				await $`git commit --amend --no-verify --no-edit`
 				process.env.AMENDING = undefined
 			}
-		},
-		{
-			name: 'post-install',
-			description: 'Runs the post-install hook',
-			args: z.object({}),
-			handler: async () => {
-				const projectRoot = process.cwd()
-				const pkgJsonPath = join(projectRoot, 'package.json')
-
-				const pkgJson = await Bun.file(pkgJsonPath).json()
-
-				pkgJson['simple-git-hooks'] = { 'post-commit': 'rubriclab-package post-commit' }
-
-				pkgJson.scripts = pkgJson.scripts || {}
-				pkgJson.scripts.prepare = 'rubriclab-package prepare'
-				pkgJson.scripts.bleed = 'bun x npm-check-updates -u'
-				pkgJson.scripts.clean = 'rm -rf .next && rm -rf node_modules'
-				pkgJson.scripts.format = 'bun x biome format --write .'
-				pkgJson.scripts.lint = 'bun x biome check .'
-				pkgJson.scripts['lint:fix'] = 'bun x biome lint . --write --unsafe'
-				pkgJson.publishConfig = { access: 'public' }
-
-				const file = Bun.file(join(import.meta.dir, 'workflows/publish-package.yml'))
-
-				await Promise.all([
-					Bun.write(pkgJsonPath, JSON.stringify(pkgJson, null, 2)),
-					Bun.write(join(projectRoot, '.github/workflows/publish.yml'), file)
-				])
-
-				console.log('Set up package successfully.')
-			}
 		}
+		// {
+		// 	name: 'post-install',
+		// 	description: 'Runs the post-install hook',
+		// 	args: z.object({}),
+		// 	handler: async () => {
+		// 		const projectRoot = process.cwd()
+		// 		const pkgJsonPath = join(projectRoot, 'package.json')
+
+		// 		const pkgJson = await Bun.file(pkgJsonPath).json()
+
+		// 		pkgJson['simple-git-hooks'] = { 'post-commit': 'rubriclab-package post-commit' }
+
+		// 		pkgJson.scripts = pkgJson.scripts || {}
+		// 		// pkgJson.scripts.prepare = 'rubriclab-package prepare'
+		// 		pkgJson.scripts.bleed = 'bun x npm-check-updates -u'
+		// 		pkgJson.scripts.clean = 'rm -rf .next && rm -rf node_modules'
+		// 		pkgJson.scripts.format = 'bun x biome format --write .'
+		// 		pkgJson.scripts.lint = 'bun x biome check .'
+		// 		pkgJson.scripts['lint:fix'] = 'bun x biome lint . --write --unsafe'
+		// 		pkgJson.publishConfig = { access: 'public' }
+
+		// 		const file = Bun.file(join(import.meta.dir, 'workflows/publish-package.yml'))
+
+		// 		await Promise.all([
+		// 			Bun.write(pkgJsonPath, JSON.stringify(pkgJson, null, 2)),
+		// 			Bun.write(join(projectRoot, '.github/workflows/publish.yml'), file)
+		// 		])
+
+		// 		console.log('Set up package successfully.')
+		// 	}
+		// }
 	]
 })
 
